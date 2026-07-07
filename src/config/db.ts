b164aa2a294be0380301fffa7a67bd6e25a4e8fc
Rup-001 <mohaimin.rup.0@gmail.com>
2026-07-07 22:52:56 +0600

    mongodb

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,18 +1,23 @@
-// const dns = require('dns');
-// dns.setServers(['8.8.8.8', '8.8.4.4']);
-
 import mongoose from 'mongoose';
 import { config } from './env';
 import logger from './logger';
+import dns from 'node:dns';   
+
+
+dns.setServers(['1.1.1.1', '8.8.8.8']);
 
 export const connectDB = async (): Promise<void> => {
   try {
-    console.log(config.mongoose.url);
-    await mongoose.connect(config.mongoose.url);
+    console.log('Connecting to:', config.mongoose.url);
+    await mongoose.connect(config.mongoose.url, {
+     
+      maxPoolSize: 5,
+      serverSelectionTimeoutMS: 10000,
+    });
     logger.info('MongoDB connected');
   } catch (error) {
-    console.log(error);
+    console.error(error);
     logger.error('MongoDB connection error');
-    process.exit(1);
+    throw error;  
   }
 };
