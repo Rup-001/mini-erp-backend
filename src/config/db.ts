fbf5e432eb8858f81189482154ecd67c5741e104
Rup-001 <mohaimin.rup.0@gmail.com>
2026-07-07 22:13:47 +0600

    mongodb

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,5 @@
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+// const dns = require('dns');
+// dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 import mongoose from 'mongoose';
 import { config } from './env';
